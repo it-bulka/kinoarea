031823dc261bde530a8a008ba02a11711e4dab03
IT.bulka <i.it.bulka@gmail.com>
2026-06-14 13:10:19 +0200

    chore(mock): remove unused persons mock data file

--- src/mock/person.ts
+++
@@ -1,9 +0,0 @@
-import Person from '../assets/images/films/person.png'
-export const personsList = [
-  { id: '1', img: Person, actor: 'Квентин Тарантино', originalActorName: 'Quentin Tarantino', age: 57, rate: 2 },
-  { id: '2', img: Person, actor: 'Квентин Тарантино', originalActorName: 'Quentin Tarantino', age: 49, rate: 2 },
-  { id: '3', img: Person, actor: 'Квентин Тарантино', originalActorName: 'Quentin Tarantino', age: 13, rate: 2 },
-  { id: '4', img: Person, actor: 'Квентин Тарантино', originalActorName: 'Quentin Tarantino', age: 99, rate: 2 },
-  { id: '5', img: Person, actor: 'Квентин Тарантино', originalActorName: 'Quentin Tarantino', age: 104, rate: 2 },
-  { id: '6', img: Person, actor: 'Квентин Тарантино', originalActorName: 'Quentin Tarantino', age: 8, rate: 2 },
-]
