312b1ea97cafedf81b1c1d5496630d5f5b9646e3
IT.bulka <i.it.bulka@gmail.com>
2026-06-16 23:17:31 +0200

    fix(film-posters): fix Prettier formatting in FilmPostersSkeleton

--- src/pages/FilmPosters/FilmPostersSkeleton.tsx
+++ src/pages/FilmPosters/FilmPostersSkeleton.tsx
@@ -15,9 +15,7 @@ export const FilmPostersSkeleton = () => (
       </div>
     </div>
 
-    <div
-      className={'grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 md:gap-4'}
-    >
+    <div className={'grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 md:gap-4'}>
       {Array.from({ length: ITEMS }).map((_, i) => (
         <Skeleton key={i} className={'w-full aspect-[2/3] rounded-10'} />
       ))}
