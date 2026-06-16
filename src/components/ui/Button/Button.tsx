14f3263b76ca39ed24a0207c79dd8d605d2916fe
IT.bulka <i.it.bulka@gmail.com>
2026-06-16 09:54:51 +0200

    refactor(typography): migrate Typography and Button components to new design system

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -2,7 +2,7 @@ import { ButtonHTMLAttributes, PropsWithChildren } from 'react'
 import cls from './Button.module.scss'
 import { twMerge } from 'tailwind-merge'
 
-type BtnVariant = 'icon' | 'primary' | 'transparent' | 'white' | 'yellow'
+type BtnVariant = 'icon' | 'primary' | 'secondary' | 'ghost' | 'transparent' | 'white'
 type BtnSizes = 'normal' | 'md' | 'sm'
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   onClick?: () => void
