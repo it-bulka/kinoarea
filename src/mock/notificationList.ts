f7ef85e936f7064c7edbe5841b87dd4ea43933d9
IT.bulka <i.it.bulka@gmail.com>
2026-06-18 10:46:46 +0200

    chore(i18n): add review, comment and notification translation keys

--- src/mock/notificationList.ts
+++ src/mock/notificationList.ts
@@ -3,5 +3,7 @@ export const notificationList = {
   userAbsentComment: 'notifications.userAbsentComment',
   userAbsentFavourite: 'notifications.userAbsentFavourite',
   commentSent: 'notifications.commentSent',
+  commentError: 'notifications.commentError',
+  commentDeleted: 'notifications.commentDeleted',
   subscriptionSent: 'notifications.subscriptionSent',
 }
